488536d43775eee8c5a3387ab0ec859410714f74
BlackRavenMomon <ashishbelel70@gmail.com>
2026-04-27 20:58:54 +0530

    setting up structure

---
+++ backend/minetracker/src/main/java/com/greenmine/minetracker/MinetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.greenmine.minetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinetrackerApplication.class, args);
+	}
+
+}
---
+++ backend/minetracker/src/test/java/com/greenmine/minetracker/MinetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenmine.minetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
